b3cc84a642eb95f54388159fecd682498895d2d9
Pierre Wacrenier <mota@souitom.org>
2015-07-24 12:12:31 +0200

    Python 2 compatibility + Pep8 compliance

--- millipede.py
+++ millipede.py
@@ -1,40 +1,56 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Print a gorgeous millipede
+"""
+
+from __future__ import print_function
+from argparse import ArgumentParser
 
 
-class millipede:
+def millipede(size, comment=None, reverse=False):
+    """
+    Output the millipede
+    """
+    padding_offsets = [2, 1, 0, 1, 2, 3, 4, 4, 3]
 
-    def __init__(self, size, comment=None, reverse=False):
-        self._padding_offsets = [2, 1, 0, 1, 2, 3, 4, 4, 3]
+    head = "    ╔⊙ ⊙╗\n" if reverse else "    ╚⊙ ⊙╝\n"
+    body = "".join([
+        "{}{}\n".format(
+            " " * padding_offsets[(x + 3) % 9 if reverse else x % 9],
+            "╔═(███)═╗" if reverse else "╚═(███)═╝"
+        )
+        for x in range(size)
+    ])
 
-        head = "    ╔⊙ ⊙╗\n" if reverse else "    ╚⊙ ⊙╝\n"
-        body = "".join([
-            "{}{}\n".format(
-                " " * self._padding_offsets[(x + 3) % 9 if reverse else x % 9],
-                "╔═(███)═╗" if reverse else "╚═(███)═╝"
-            )
-            for x in range(size)
-        ])
+    output = ""
+    if reverse:
+        output += body + head
+        if comment:
+            output += "\n" + comment
+    else:
+        if comment:
+            output += comment + "\n\n"
+        output += head + body
 
-        self._millipede = ""
-        if reverse:
-            self._millipede += body + head
-            if comment:
-                self._millipede += "\n" + comment
-        else:
-            if comment:
-                self._millipede += comment + "\n\n"
-            self._millipede += head + body
+    return output
 
-    def __str__(self):
-        return self._millipede
-
-if __name__ == "__main__":
-    import argparse
-
-    parser = argparse.ArgumentParser(description='Millipede generator')
-    parser.add_argument('size', metavar='s', type=int, help='the size of the millipede')
-    parser.add_argument('comment', metavar='c', type=str, help='the comment', nargs="?")
-    parser.add_argument('-r', '--reverse', action='store_true', help='reverse the millipede')
+def main():
+    """
+    Entry point
+    """
+    parser = ArgumentParser(description='Millipede generator')
+    parser.add_argument('size', metavar='s',
+                        type=int,
+                        help='the size of the millipede')
+    parser.add_argument('comment', metavar='c',
+                        help='the comment', nargs="?")
+    parser.add_argument('-r', '--reverse',
+                        action='store_true',
+                        help='reverse the millipede')
     args = parser.parse_args()
 
     print(millipede(args.size, comment=args.comment, reverse=args.reverse))
+
+if __name__ == "__main__":
+    main()
