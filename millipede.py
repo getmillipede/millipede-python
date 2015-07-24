dbbd6e1e87964db6b2279a661a63751da31213e5
Bertrand Marron <bertrand.marron@ionisx.com>
2015-07-24 12:16:34 +0200

    Rewrite body generation and add reverse option

--- millipede.py
+++ millipede.py
@@ -2,26 +2,28 @@
 
 
 class millipede:
-    def __init__(self, size, comment=None):
+
+    def __init__(self, size, comment=None, reverse=False):
+        self._padding_offsets = [2, 1, 0, 1, 2, 3, 4, 4, 3]
+
+        head = "    ╔⊙ ⊙╗\n" if reverse else "    ╚⊙ ⊙╝\n"
+        body = "".join([
+            "{}{}\n".format(
+                " " * self._padding_offsets[(x + 3) % 9 if reverse else x % 9],
+                "╔═(███)═╗" if reverse else "╚═(███)═╝"
+            )
+            for x in range(size)
+        ])
+
         self._millipede = ""
-        if comment:
-            self._millipede = comment + "\n\n"
-
-        self._millipede += "    ╚⊙ ⊙╝ \n"
-        padding = 2
-        direction = -1
-        while (size):
-            for i in range(0, padding):
-                self._millipede += " "
-            self._millipede += "╚═(███)═╝\n"
-            padding += direction
-
-            if padding == 0:
-                direction = 1
-            elif padding == 4:
-                padding = 3
-                direction = -1
-            size -= 1
+        if reverse:
+            self._millipede += body + head
+            if comment:
+                self._millipede += "\n" + comment
+        else:
+            if comment:
+                self._millipede += comment + "\n\n"
+            self._millipede += head + body
 
     def __str__(self):
         return self._millipede
@@ -32,6 +34,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Millipede generator')
     parser.add_argument('size', metavar='s', type=int, help='the size of the millipede')
     parser.add_argument('comment', metavar='c', type=str, help='the comment', nargs="?")
+    parser.add_argument('-r', '--reverse', action='store_true', help='reverse the millipede')
     args = parser.parse_args()
 
-    print(millipede(args.size, args.comment))
+    print(millipede(args.size, comment=args.comment, reverse=args.reverse))
